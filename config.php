8bf21680662774af6f817b5ad7391ca8aad1ad68
drashtidevani <drashtidevani88@gmail.com>
2022-08-15 16:02:26 +0530

    php web library management system 15-8-22

---
+++ config.php
@@ -0,0 +1,16 @@
+
+
+<?php 
+
+$server = "localhost";
+$user = "root";
+$pass = "";
+$database = "login_register";
+
+$conn = mysqli_connect($server, $user, $pass, $database);
+
+if (!$conn) {
+    die("alert('Connection Failed.')");
+}
+
+?>
